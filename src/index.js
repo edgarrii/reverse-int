932ebca985be4061a0804d50cae8167f359a3b62
edgarrii <71753826+edgarrii@users.noreply.github.com>
2021-02-28 14:52:38 +0300

    feat: add new body to function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+  if (number < 0) {
+    number = -1 * number;
+  }
+   return +number.toString().split('').reverse().join('');
 }
+
